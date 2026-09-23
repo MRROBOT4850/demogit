e12265bb4303ee625d27ec566204b173fe498055
Shivam Chaudhary-dev <113339908+MRROBOT4850@users.noreply.github.com>
2026-09-23 22:49:41 +0530

    Add about route to Express application

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ app.get("/", (req, res) => {
   res.send("Hello from Express!");
 });
 
-
+app.get("/about", (req, res) => {
+  res.send("This is the about page.");
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
