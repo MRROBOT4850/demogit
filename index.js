9ab0a6073573e9ba8e070b9c07b0c381b54f728e
MRROBOT4850 <shivamchaudhary4850@gmail.com>
2026-09-23 22:46:34 +0530

    Add about route

--- index.js
+++ index.js
@@ -8,9 +8,7 @@ app.get("/", (req, res) => {
   res.send("Hello from Express!");
 });
 
-app.get("/about", (req, res) => {
-  res.send("This is the about page.");
-});
+
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
